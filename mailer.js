6549b14e592a67351cbc29b4c817dfab9ed901e4
dcars18 <deca222@g.uky.edu>
2017-04-10 13:39:19 -0400

    commit

--- mailer.js
+++ mailer.js
@@ -45,35 +45,55 @@ module.exports = {
 	},
 	formatSARAEmail: function(userData){
 		mailOptions.text = ""
+		mailOptions.html = "<ul>"
 		if(userData.firstname != null && userData.lastname != null)
 		{
 			mailOptions.text += "Name: " + userData.firstname + " " + userData.lastname + "\n";
+			mailOptions.html += "<li>Name: " + userData.firstname + " " + userData.lastname;
+
+		}
+		if(userData.location != null)
+		{
+			mailOptions.text += "Location: " + userData.location +"\n";
+			mailOptions.html += "<li>Location: " + userData.location;
 		}
 		if(userData.date != null)
 		{
 			mailOptions.text += "Date: " + userData.date +"\n";
+			mailOptions.html += "<li>Date: " + userData.date;
 		}
 		if(userData.email != null)
 		{
 			mailOptions.text += "Email: " + userData.email + "\n";
+			mailOptions.html += "<li>Email: " + userData.email;
+		}
+		if(userData.phone != null)
+		{
+			mailOptions.text += "Phone #: " + userData.phone + "\n";
+			mailOptions.html += "<li>Phone #: " + userData.phone;
 		}
 		if(userData.toggleCharged != null)
 		{
 			mailOptions.text += "Press Charges: " + userData.toggleCharged + "\n";
+			mailOptions.html += "<li>Press Charges: " + userData.toggleCharged;
 		}
 		if(userData.toggleEvidence != null)
 		{
-			mailOptions.text += "Collect Evidence: " + userData.toggleCharged + "\n";
+			mailOptions.text += "Collect Evidence: " + userData.toggleEvidence + "\n";
+			mailOptions.html += "<li>Collect Evidence: " + userData.toggleEvidence + "\n";
 		}
 		if(userData.toggleMedical)
 		{
-			mailOptions.text += "Requires Medical Attention: " + userData.toggleCharged + "\n";
+			mailOptions.text += "Requires Medical Attention: " + userData.toggleMedical + "\n";
+			mailOptions.html += "<li>Requires Medical Attention: " + userData.toggleMedical;
 		}
 		if(userData.toggleRelocation)
 		{
-			mailOptions.text += "Needs to be relocated: " + userData.toggleCharged + "\n";
+			mailOptions.text += "Needs to be relocated: " + userData.toggleRelocation + "\n";
+			mailOptions.html += "<li>Needs to be relocated: " + userData.toggleRelocation;
 		}
-
+		//mailOptions.html += mailOptions.text
+		mailOptions.html += "</ul>"
 		this.sendMail("deca222@g.uky.edu");
 	}
 };
