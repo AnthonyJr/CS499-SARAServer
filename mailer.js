6b67d9148e0a1f2f4b8a4c461deab38f5ba58193
dcars18 <deca222@g.uky.edu>
2017-04-23 17:26:24 -0400

    Fixes some stuff with emails and adds email anonimity with the emailInfo.txt file

--- mailer.js
+++ mailer.js
@@ -1,14 +1,33 @@
+//This file does all the work for sending the email out to the authorities
+//Required files gmailappinfo.txt, a file with username and password for the SARA Account
+//emailInfo.txt, a file with all of the authority emails we will be contacting.
+
 'use strict';
 const nodemailer = require('nodemailer');
 
-/* 
-*  Get the app info here because I don't want this to be on GitHub
-*  Ask Dave for this information and don't upload the file to GitHub...
-*  I will deny your pull request if you have this info in your directory.
-*/
+//NOTES:
+/*
+	To add more emails simply add three new entries in the emailInfo.txt file.
+	This will then use logic below to associate which emails go to which locations.
+	There may be a better way of doing this down the road but I think this is a very easy way
+	To allow for quick expansion of app. Add logic below for another location and add emails to text file
+	Give user another option in the front end and youre done.
+ */
 var fs = require('fs');
 var gmailAppInfo = fs.readFileSync('gmailappinfo.txt', 'utf8');
 gmailAppInfo = gmailAppInfo.split('\n');
+
+//Sets up email addresses for emailing authorities, emails are in files to avoid data leakage
+//And most importantly adds the ability to add more emails based on location.
+var emailAddresses = [];
+var temp = fs.readFileSync('emailInfo.txt', 'utf8');
+temp = temp.split('\n');
+temp.forEach(function(emailAddress){
+	if(emailAddress != "")
+	{
+		emailAddresses.push(emailAddress);
+	}
+})
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
@@ -20,6 +39,33 @@ let transporter = nodemailer.createTransport({
 });
 
 // setup email data with unicode symbols
+// The To will be switched off my email when its time for production...
+// Read from file for privacy of email
+// Set to default values of Lexington...
+let policeMailOptions = {
+	from: '"SARA App Alerts" <SARAAppAlerts@gmail.com>', // sender address
+	to: emailAddresses[0], // list of receivers
+	subject: 'SARA Report Generated', // Subject line
+	text: 'Hello world ?', // plain text body
+	html: '<b>Hello world ?</b>' // html body
+};
+
+let womensShelterMailOptions = {
+	from: '"SARA App Alerts" <SARAAppAlerts@gmail.com>', // sender address
+	to: emailAddresses[1], // list of receivers
+	subject: 'SARA Report Generated', // Subject line
+	text: 'Hello world ?', // plain text body
+	html: '<b>Hello world ?</b>' // html body
+};
+
+let hospitalMailOptions = {
+	from: '"SARA App Alerts" <SARAAppAlerts@gmail.com>', // sender address
+	to: emailAddresses[2], // list of receivers
+	subject: 'SARA Report Generated', // Subject line
+	text: 'Hello world ?', // plain text body
+	html: '<b>Hello world ?</b>' // html body
+};
+
 let mailOptions = {
 	from: '"SARA App Alerts" <SARAAppAlerts@gmail.com>', // sender address
 	to: '', // list of receivers
@@ -28,6 +74,7 @@ let mailOptions = {
 	html: '<b>Hello world ?</b>' // html body
 };
 
+var mailArr = [policeMailOptions, hospitalMailOptions, womensShelterMailOptions];
 
 module.exports = {
 	// send mail with defined transport object
@@ -44,63 +91,128 @@ module.exports = {
 		console.log(mailOptions.to);
 	},
 	formatSARAEmail: function(userData, callback){
-		mailOptions.text = ""
-		mailOptions.html = "<ul>"
+		//Set up mail for all three objects
+		mailArr.forEach(function(mailOption){
+			mailOption.text = ""
+			mailOption.html = "<ul>"
+		})
+
+		//Put the firstname and lastname on each object
 		if(userData.firstname != undefined && userData.lastname != undefined)
 		{
-			mailOptions.text += "Name: " + userData.firstname + " " + userData.lastname + "\n";
-			mailOptions.html += "<li>Name: " + userData.firstname + " " + userData.lastname;
-
+			mailArr.forEach(function(mailOption){
+				mailOption.text += "Name: " + userData.firstname + " " + userData.lastname + "\n";
+				mailOption.html += "<li>Name: " + userData.firstname + " " + userData.lastname;
+			})
 		}
+		//Put location on each object, also decides the email receiver
 		if(userData.location != undefined)
 		{
-			mailOptions.text += "Location: " + userData.location +"\n";
-			mailOptions.html += "<li>Location: " + userData.location;
+			if(userData.location == "Lexington")
+			{
+				for(var i = 0; i < mailArr.length; i++)
+				{
+					mailArr[i].to = emailAddresses[i]
+				}
+			}
+
+			mailArr.forEach(function(mailOption){
+				mailOption.text += "Location: " + userData.location +"\n";
+				mailOption.html += "<li>Location: " + userData.location;
+			});
+
 		}
+		//Put date if it exists
 		if(userData.date != undefined)
 		{
-			mailOptions.text += "Date of incident: " + userData.date +"\n";
-			mailOptions.html += "<li>Date of incident: " + userData.date;
+			mailArr.forEach(function(mailOption){
+				mailOption.text += "Date of incident: " + userData.date +"\n";
+				mailOption.html += "<li>Date of incident: " + userData.date;
+			});
+
 		}
+		//Put email if it exists
 		if(userData.email != undefined)
 		{
-			mailOptions.text += "Email: " + userData.email + "\n";
-			mailOptions.html += "<li>Email: " + userData.email;
+			mailArr.forEach(function(mailOption){
+				mailOption.text += "Email: " + userData.email + "\n";
+				mailOption.html += "<li>Email: " + userData.email;
+			});
+
 		}
+		//Put phone if it exists
 		if(userData.phone != undefined)
 		{
-			mailOptions.text += "Phone #: " + userData.phone + "\n";
-			mailOptions.html += "<li>Phone #: " + userData.phone;
+			mailArr.forEach(function(mailOption){
+				mailOption.text += "Phone #: " + userData.phone + "\n";
+				mailOption.html += "<li>Phone #: " + userData.phone;
+			});
+
 		}
-		if(userData.toggleCharged != undefined)
+		//Check for evidence or press charges to exist, if they exist check if they are true.
+		//Add to the email if they are true and send it out
+		if((userData.toggleEvidence != undefined) || (user.toggleCharged != undefined))
 		{
-			mailOptions.text += "Press Charges: " + userData.toggleCharged + "\n";
-			mailOptions.html += "<li>Press Charges: " + userData.toggleCharged;
+			if(userData.toggleEvidence == true || userData.toggleCharged == true)
+			{
+				if(userData.toggleCharged == true)
+				{
+					mailArr[0].text += "Press Charges: " + userData.toggleCharged + "\n";
+					mailArr[0].html += "<li>Press Charges: " + userData.toggleCharged;
+				}
+				if(userData.toggleEvidence == true)
+				{
+					mailArr[0].text += "Collect Evidence: " + userData.toggleEvidence + "\n";
+					mailArr[0].html += "<li>Collect Evidence: " + userData.toggleEvidence;
+				}
+				mailArr[0].html += "</ul>"
+
+				transporter.sendMail(mailArr[0], (error, info) => {
+				if (error) {
+					callback(error);
+				}
+				console.log('Police Message %s sent: %s', info.messageId, info.response);
+				callback(null);
+				})
+			}
 		}
-		if(userData.toggleEvidence != undefined)
-		{
-			mailOptions.text += "Collect Evidence: " + userData.toggleEvidence + "\n";
-			mailOptions.html += "<li>Collect Evidence: " + userData.toggleEvidence + "\n";
-		}
+		//Check if medical is true and if it is send email
 		if(userData.toggleMedical != undefined)
 		{
-			mailOptions.text += "Requires Medical Attention: " + userData.toggleMedical + "\n";
-			mailOptions.html += "<li>Requires Medical Attention: " + userData.toggleMedical;
+			if(userData.toggleMedical == true)
+			{
+				mailArr[1].text += "Requires Medical Attention: " + userData.toggleMedical + "\n";
+				mailArr[1].html += "<li>Requires Medical Attention: " + userData.toggleMedical + "</ul>";
+
+				transporter.sendMail(mailArr[1], (error, info) => {
+				if (error) {
+					callback(error);
+				}
+				console.log('Medical Message %s sent: %s', info.messageId, info.response);
+				callback(null);
+				})
+			}
 		}
+		//Check if relocation is true and if it is send email
 		if(userData.toggleRelocation != undefined)
 		{
-			mailOptions.text += "Needs to be relocated: " + userData.toggleRelocation + "\n";
-			mailOptions.html += "<li>Needs to be relocated: " + userData.toggleRelocation;
+			if(userData.toggleRelocation == true)
+			{
+				mailArr[2].text += "Needs to be relocated: " + userData.toggleRelocation + "\n";
+				mailArr[2].html += "<li>Needs to be relocated: " + userData.toggleRelocation + "</ul>";
+
+				transporter.sendMail(mailArr[2], (error, info) => {
+				if (error) {
+					callback(error);
+				}
+				console.log('Relocation Message %s sent: %s', info.messageId, info.response);
+				callback(null);
+				})
+			}
 		}
-		//mailOptions.html += mailOptions.text
-		mailOptions.html += "</ul>"
-		mailOptions.to = "deca222@g.uky.edu"
-		transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			callback(error);
-		}
-			console.log('Message %s sent: %s', info.messageId, info.response);
-			callback(null);
-		})
+
+		//If theyre all false send back empty callback....
+		callback(null);
+
 	}
 };
